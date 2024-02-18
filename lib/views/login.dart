c45609408814ecf027a6fcc72bdcab6614351cc4
Getonto3 <getontomoses18@gmail.com>
2024-02-18 19:05:17 +0300

    feat: login page

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,69 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:quotes/configs/constants.dart';
 import 'package:quotes/views/customtext.dart';
-import 'package:quotes/views/customTextField.dart';
+import 'package:quotes/configs/constants.dart';
+import 'package:quotes/views/customtextField.dart';
+import 'package:quotes/views/customButton.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class LogIn extends StatelessWidget {
+  const LogIn({super.key});
 
   @override
   Widget build(BuildContext context) {
     final TextEditingController usernameController = TextEditingController();
     final TextEditingController passwordController = TextEditingController();
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: const CustomText(
-            label: "Airline Booking",
-            labelColor: appWhiteColor,
-            fontSize: 28,
-          ),
-        ),
-        backgroundColor: Colors.redAccent,
-        foregroundColor: appWhiteColor,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Center(
-                child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: CustomText(
-                    label: "Login Screen",
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+        appBar: AppBar(
+            title: Center(
+              child: CustomText(
+                label: 'AIRLINE BOOKING',
+                labelcolor: appWhiteColor,
+              ),
+            ),
+            backgroundColor: Color.fromARGB(255, 56, 181, 150)),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CustomText(
+                      label: 'Log In',
+                      fontsize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: CustomText(
-                  label: "Username",
-                  labelColor: Colors.blue,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CustomText(label: "Email", labelcolor: Colors.blue),
                 ),
-              ),
-              CustomTextField(
-                  Controller: usernameController,
-                  hintText: "Enter your username",
-                  prefixIcon: const Icon(Icons.person)),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: CustomText(label: "Password"),
-              ),
-              CustomTextField(
-                Controller: passwordController,
-                hintText: "Enter your password",
-                prefixIcon: const Icon(Icons.lock),
-                suffixIcon: const Icon(Icons.visibility),
-              ),
-              const SizedBox(height: 20),
-              // const CustomTextButton(
-              //   buttonName: "Log In",
-              // )
-            ]),
-      ),
-    );
+                CustomTextField(
+                  controller: usernameController,
+                  hintText: "Enter Email",
+                  prefixIcon: Icon(Icons.person),
+                  suffixIcon: Icon(null),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CustomText(label: "Password", labelcolor: Colors.blue),
+                ),
+                CustomTextField(
+                  controller: usernameController,
+                  hintText: "Enter Password",
+                  prefixIcon: Icon(Icons.person),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                ),
+                const SizedBox(height: 20),
+                CustomButton(
+                  label: "Sign In",
+                  onPressed: () {},
+                  buttonColor: Color.fromARGB(255, 56, 181, 150),
+                  width: 18,
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
