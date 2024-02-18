d136fc9f81b7b00ca6de238e84266af333a10225
Getonto3 <getontomoses18@gmail.com>
2024-02-18 17:58:16 +0300

    feat: register login in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:quotes/views/login.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: QuoteList(),
+    home: Login(),
+    debugShowCheckedModeBanner: false,
   ));
-}
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({super.key});
-
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    'The truth is rarely pure and never simple',
-    'God is good all the time',
-    'Discipline is the bridge between goals and achievement'
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          title: Text("Awesome Quotes"),
-          centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 50, 228, 207),
-        ),
-        body: Column(
-          children: quotes.map((quote) => Text(quote)).toList(),
-        ));
-  }
 }
