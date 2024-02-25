c339359268f487353945e8fdd4ce7e8caf48f9fb
Getonto3 <getontomoses18@gmail.com>
2024-02-26 02:05:33 +0300

    feat:project utilities

---
+++ lib/utils/route.dart
@@ -0,0 +1,15 @@
+import "package:get/get.dart";
+
+import 'package:quotes/views/login.dart';
+import 'package:quotes/views/signup.dart';
+import 'package:quotes/views/dashboard.dart';
+import 'package:quotes/views/home.dart';
+
+class Routes {
+  static var routes = [
+    GetPage(name: '/', page: () => LogIn()),
+    GetPage(name: '/home', page: () => Home()),
+    GetPage(name: '/signup', page: () => SignUp()),
+    GetPage(name: '/dashboard', page: () => Dashboard()),
+  ];
+}
