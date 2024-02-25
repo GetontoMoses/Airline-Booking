d08d53e8bd6ed6283b4fb67973bb57204154faec
Getonto3 <getontomoses18@gmail.com>
2024-02-25 20:52:40 +0300

    feat: project header file

---
+++ lib/views/Head.dart
@@ -0,0 +1,218 @@
+import 'package:flutter/material.dart';
+import 'package:quotes/views/customtext.dart';
+
+Widget head() {
+  return Stack(
+    children: [
+      Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 240,
+            decoration: const BoxDecoration(
+              color: Color(0xff368983),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 35,
+                  left: 305,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(7),
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      color: const Color.fromRGBO(250, 250, 250, 0.1),
+                      child: const Icon(
+                        Icons.notification_add_outlined,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 30, left: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Current Location',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 224, 223, 223)),
+                      ),
+                      Text(
+                        'Athi River, Kenya',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 20,
+                            color: Color.fromARGB(255, 224, 223, 223)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      Positioned(
+        top: 140,
+        left: 25,
+        child: Container(
+          height: 170,
+          width: 360,
+          decoration: BoxDecoration(
+            boxShadow: const [
+              BoxShadow(
+                color: Color.fromRGBO(47, 125, 121, 0.3),
+                offset: Offset(0, 6),
+                blurRadius: 12,
+                spreadRadius: 6,
+              ),
+            ],
+            color: const Color.fromARGB(255, 47, 125, 121),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: const Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total Balance',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Icon(
+                      Icons.more_horiz,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 7,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: Row(
+                  children: [
+                    Text(
+                      '\$ 2,954',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 13,
+                          backgroundColor: Color.fromARGB(255, 85, 145, 141),
+                          child: Icon(
+                            Icons.arrow_downward,
+                            color: Colors.white,
+                            size: 19,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 7,
+                        ),
+                        Text(
+                          'Income',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 13,
+                          backgroundColor: Color.fromARGB(255, 85, 145, 141),
+                          child: Icon(
+                            Icons.arrow_upward,
+                            color: Colors.white,
+                            size: 19,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 7,
+                        ),
+                        Text(
+                          'Expenses',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 7,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '\$ 1,450',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      '\$ 450',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ],
+  );
+}
