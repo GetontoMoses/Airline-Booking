e46d13ae5778db10ab2aa924d3fc03b6b928c7cd
Getonto3 <getontomoses18@gmail.com>
2024-03-04 13:37:45 +0300

    recent

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -1,41 +1,50 @@
-import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'dashboard.dart';
 
-import 'package:quotes/controllers/homecontroller.dart';
+import 'package:quotes/controllers/HomeController.dart';
+import 'package:quotes/views/Dashboard.dart';
+import 'package:quotes/views/search.dart';
+import 'package:quotes/views/tickets.dart';
+
+
 
 HomeController homeController = Get.put(HomeController());
-var Screen = [Dashboard()];
+var Screen = [Dashboard(), Search(), Tickets()];
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Floating NavBar Example',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Floating NavBar Example'),
-          centerTitle: true,
-        ),
-        //If you want to show body behind the navbar, it should be true
-        extendBody: true,
-        body: Obx(
-          () => Center(child: Screen[homeController.selectedPage.value]),
-        ),
-        bottomNavigationBar: FloatingNavbar(
-          onTap: (index) {
-            homeController.updateSelectedPage(index);
-          },
-          items: [
-            FloatingNavbarItem(icon: Icons.home, title: 'Home'),
-            FloatingNavbarItem(icon: Icons.explore, title: 'Explore'),
-            FloatingNavbarItem(icon: Icons.chat_bubble_outline, title: 'Chats'),
-            FloatingNavbarItem(icon: Icons.settings, title: 'Settings'),
-          ],
-        ),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: homeController.selectedPage.value,
+        onTap: (index) {
+          homeController.selectedPage.value = index;
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.download_rounded),
+            label: 'tickets',
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Obx(
+              () => Center(child: Screen[homeController.selectedPage.value]),
+            ),
+          ),
+        ],
       ),
     );
   }
